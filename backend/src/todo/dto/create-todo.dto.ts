012cbe7b7297718e2e036b78deb0b6d86efc1ba1
squarednalytic <65530291+squarednalytic@users.noreply.github.com>
2026-01-06 15:50:35 +0700

    Add files via upload

--- backend/src/todo/dto/create-todo.dto.ts
+++ backend/src/todo/dto/create-todo.dto.ts
@@ -1,1 +1,7 @@
+import { IsString, MinLength } from 'class-validator';
 
+export class CreateTodoDto {
+  @IsString()
+  @MinLength(1, { message: 'Title must be at least 1 character' })
+  title: string;
+}
